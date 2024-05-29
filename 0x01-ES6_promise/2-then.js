fe4a8af552bc81a167049056b09f2a5c59b16d66
Demidorn <dornkaizen@gmail.com>
2024-05-29 17:23:35 +0300

    promise: upload photo

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,5 @@
-/* eslint-disable jest/require-hook */
-/* eslint-disable no-unused-vars */
 function handleResponseFromAPI(promise) {
   return promise.then({ status: 200, body: 'success' });
-                .catch(new Error(''));
+  .catch(new Error(''));
                 .finally('Got a response from the API');
 }
