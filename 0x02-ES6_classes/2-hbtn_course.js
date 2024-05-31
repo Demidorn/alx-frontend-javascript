231b186824e64f02471a3821b6a6b6957420ae3b
Demidorn <dornkaizen@gmail.com>
2024-05-31 19:28:46 +0300

    updated

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,45 +1,45 @@
 class HolbertonCourse {
-    constructor(name, length, students) {
-        this._name = name;
-        this._length = length;
-        this._students = students;
-    }
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-    get name() {
-        return this._name;
-    }
-    set name(newName) {
-        if (typeof newName === 'string') {
-            this._name = newName;
-        } else {
-            throw new TypeError('Name must be a string');
+  get name() {
+    return this._name;
+  }
 
-        }
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
     }
+  }
 
-    get length() {
-        return this._length;
-    }
-    set length(newLength) {
-        if (typeof newLength !== 'number') {
-            throw new TypeError('Length must be a number');
-        } else {
+  get length() {
+    return this._length;
+  }
 
-            this._length = newLength;
-        }
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = newLength;
     }
+  }
 
-    get students() {
-        return this._students;
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(newStudents) && students.every((student) => typeof student === 'string')) {
+      this._students = newStudents;
+    } else {
+      throw new TypeError('Students must be an array of strings');
     }
-    set students(newStudents) {
-        if (Array.isArray(newStudents) && students.every((student) => typeof student === 'string')) {
-            this._students = newStudents;
-        } else {
-            throw new TypeError('Students must be an array of strings');
-        }
-    }
-    
+  }
 }
 
 export default HolbertonCourse;
