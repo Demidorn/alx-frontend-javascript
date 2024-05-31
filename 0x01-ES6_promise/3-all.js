30ab8231fb4cf63c248c8933b3a47ae7bc92520f
Demidorn <dornkaizen@gmail.com>
2024-05-31 17:11:46 +0300

    promise: handle profile signup

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,23 +1,12 @@
-function uploadPhoto() {
-  return Promise.resolve({
-    status: 200,
-    body: 'photo-profile-1',
-  });
-}
-
-function createUser() {
-  return Promise.resolve({
-    firstName: 'Guillaume',
-    lastName: 'Salva',
-  });
-}
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
   const promises = ([uploadPhoto(), createUser()]);
   Promise.all(promises).then((values) => {
     console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
   })
-    .catch((error) => console.error('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
+    return promises;
 }
 
 export default handleProfileSignup;
