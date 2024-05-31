c3e2c2a12621da581ed2dec8f38fef3bf57e394e
Demidorn <dornkaizen@gmail.com>
2024-05-31 17:22:52 +0300

    promise: function with rejecting error

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,11 +1,3 @@
-// export default function uploadPhoto(filename) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       reject(new Error(`${filename} cannot be processed`));
-//     }, 1000);
-//   });
-// }
-
 export default function uploadPhoto(filename) {
   return new Promise((resolve, reject) => {
     reject(new Error(`${filename} cannot be processed`));
