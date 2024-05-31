dce4f8ea7463a63f06224fff1902a410066ee489
Demidorn <dornkaizen@gmail.com>
2024-05-31 14:22:55 +0300

    updated

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,3 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
-    array[array.indexOf(idx)] = appendString + idx;
-  }
-
-  return array;
+  return array.map((value) => appendString + value);
 }
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,6 +1,6 @@
-import createReportObject from './12-createReportObject.js';
-import createEmployeesObject from './11-createEmployeesObject.js';
-import createIteratorObject from './100-createIteratorObject.js';
+import createReportObject from './12-createReportObject';
+import createEmployeesObject from './11-createEmployeesObject';
+import createIteratorObject from './100-createIteratorObject';
 
 export default function iterateThroughObject(reportWithIterator) {
   const employeesArray = [...reportWithIterator];
