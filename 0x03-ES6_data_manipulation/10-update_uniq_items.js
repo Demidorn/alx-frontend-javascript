b76334e9fc52eab7f24d933f0e22f82b7a018af2
Demidorn <dornkaizen@gmail.com>
2024-06-04 15:58:51 +0300

    updateUniqueItems: returns an updated map for all items with initial quantity at 1

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,11 @@
-/* eslint-disable no-unused-vars */
-function updateUniqueItems(inputMap) {
+export default function updateUniqueItems(inputMap) {
   if (!(inputMap instanceof Map)) {
     throw new Error('Cannot process');
   }
   const outputMap = new Map();
   for (const [item, quantity] of inputMap) {
     if (quantity === 1) {
-      outputMap.set(item, 100);
+      inputMap.set(item, 100);
     } else {
       outputMap.set(item, quantity + 1);
     }
